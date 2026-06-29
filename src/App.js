0995e54e6c95551a24fc8895889421b2b27e8f0a
Cool J's <live@cooljsafterdark.com>
2026-06-28 22:43:44 -0400

    Scaffold: App router

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import Calendar from './pages/Calendar';
+import Booking from './pages/Booking';
+import OpenMic from './pages/OpenMic';
+
+export default function App() {
+  return (
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/open-mic" element={<OpenMic />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
